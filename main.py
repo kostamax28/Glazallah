65925897a394f217e73c87a1e45c8b26bb6c86ec
kostamax28 <96413785+kostamax28@users.noreply.github.com>
2022-07-26 08:42:58 +0600

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ import keepalive
 
 ID = os.environ['id']
 
-bot = telebot.TeleBot(os.environ['token'])
+bot = telebot.TeleBot(os.environ['5446519593:AAHWfTBE-YJeKbJFT6l9WXROPxp87vR9mWk'])
 
 adr = ['Тверская улица, дом 13', 'Проспект 60-летия Октября', 'Улица Винокурова', '3-й Голутвинский переулок']
 
